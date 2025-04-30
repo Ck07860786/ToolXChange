c9d9f57a1839f703f761853dbf641a041d2b00c2
Chotu Kumar <ck4001252@gmail.com>
2025-04-30 17:28:49 +0530

    logo updated

--- src/components/ui/resizable-navbar.jsx
+++ src/components/ui/resizable-navbar.jsx
@@ -9,6 +9,8 @@ import {
 } from "framer-motion";
 
 import React, { useRef, useState } from "react";
+import Logo from '../assets/mainicon.png'
+import Image from "next/image";
 
 
 export const Navbar = ({
@@ -194,12 +196,12 @@ export const NavbarLogo = () => {
     (<a
       href="#"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black">
-      <img
-        src="https://assets.aceternity.com/logo-dark.png"
+      <Image
+        src={Logo}
         alt="logo"
-        width={30}
-        height={30} />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+        width={80}
+        height={80} />
+     
     </a>)
   );
 };
