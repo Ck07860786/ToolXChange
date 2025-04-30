83e8daae727d825c2124d32227e306f4facf0152
Chotu Kumar <ck4001252@gmail.com>
2025-04-30 12:57:13 +0530

    quick fixes

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -75,11 +75,11 @@ export function Hero() {
            
           <button
             className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Explore Now
+            Get Started
           </button>
           <button
             className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            Contact Support
+            Browse Tools
           </button>
           
           
