cfa29b827813e95370e9bf13b8f1f865edfc5da2
Chotu Kumar <ck4001252@gmail.com>
2025-05-02 12:21:52 +0530

    few design update

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -32,22 +32,27 @@ export function Hero() {
       <div className="px-4 py-10 md:py-20">
         <h1
           className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Share More. Spend Less. Build Better."
-            .split(" ")
-            .map((word, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
-                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
-                transition={{
-                  duration: 0.3,
-                  delay: index * 0.1,
-                  ease: "easeInOut",
-                }}
-                className="mr-2 inline-block">
-                {word}
-              </motion.span>
-            ))}
+         {
+  "Share More. Spend Less. Build Better."
+    .split(" ")
+    .map((word, index) => (
+      <motion.span
+        key={index}
+        initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+        animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+        transition={{
+          duration: 0.3,
+          delay: index * 0.1,
+          ease: "easeInOut",
+        }}
+        className={`mr-2 inline-block ${
+          word.includes("More") || word.includes("Less") ? "text-blue-500 dark:text-purple-400" : ""
+        }`}
+      >
+        {word}
+      </motion.span>
+    ))
+}
         </h1>
         <motion.p
           initial={{
